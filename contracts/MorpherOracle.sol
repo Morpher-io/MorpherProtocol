376f3b285763636ffbd74042a860c29a623fd872
Martin Froehler <45738899+Survion@users.noreply.github.com>
2021-02-01 17:30:01 +0100

    MorpherOracle.sol
    -) Added optional whitelist/blacklist
    -) Added delistings
    -) Added liquidations by Administrator
    -) Added stop loss / limit profit support

--- contracts/MorpherOracle.sol
+++ contracts/MorpherOracle.sol
@@ -2,9 +2,11 @@ pragma solidity 0.5.16;
 
 import "./Ownable.sol";
 import "./MorpherTradeEngine.sol";
+import "./MorpherState.sol";
+import "./SafeMath.sol";
 
 // ----------------------------------------------------------------------------------
-// Morpher Oracle contract
+// Morpher Oracle contract v 2.0
 // The oracle initates a new trade by calling trade engine and requesting a new orderId.
 // An event is fired by the contract notifying the oracle operator to query a price/liquidation unchecked
 // for a market/user and return the information via the callback function. Since calling
@@ -15,13 +17,24 @@ import "./MorpherTradeEngine.sol";
 contract MorpherOracle is Ownable {
 
     MorpherTradeEngine tradeEngine;
+    MorpherState state; // read only, Oracle doesn't need writing access to state
+
+    using SafeMath for uint256;
 
     bool public paused;
+    bool public useWhiteList;
 
     uint256 public gasForCallback;
+
     address payable public callBackCollectionAddress;
 
     mapping(address => bool) public callBackAddress;
+    mapping(address => bool) public whiteList;
+    
+    mapping(bytes32 => uint256) public priceBelow;
+    mapping(bytes32 => uint256) public priceAbove;
+    mapping(bytes32 => uint256) public goodFrom;
+    mapping(bytes32 => uint256) public goodUntil;
 
 // ----------------------------------------------------------------------------------
 // Events
@@ -33,7 +46,11 @@ contract MorpherOracle is Ownable {
         bool _tradeAmountGivenInShares,
         uint256 _tradeAmount,
         bool _tradeDirection,
-        uint256 _orderLeverage
+        uint256 _orderLeverage,
+        uint256 _onlyIfPriceBelow,
+        uint256 _onlyIfPriceAbove,
+        uint256 _goodFrom,
+        uint256 _goodUntil
         );
 
     event LiquidationOrderCreated(
@@ -46,6 +63,7 @@ contract MorpherOracle is Ownable {
     event OrderProcessed(
         bytes32 indexed _orderId,
         uint256 _price,
+        uint256 _adjustedPrice,
         uint256 _spread,
         uint256 _positionLiquidationTimestamp,
         uint256 _timeStamp,
@@ -64,7 +82,11 @@ contract MorpherOracle is Ownable {
         bool _tradeAmountGivenInShares,
         uint256 _tradeAmount,
         bool _tradeDirection,
-        uint256 _orderLeverage
+        uint256 _orderLeverage,
+        uint256 _onlyIfPriceBelow,
+        uint256 _onlyIfPriceAbove,
+        uint256 _goodFrom,
+        uint256 _goodUntil
         );
 
     event OrderCancelled(
@@ -96,8 +118,39 @@ contract MorpherOracle is Ownable {
         address _address
         );
 
+    event LinkMorpherState(
+        address _address
+        );
+
+    event SetUseWhiteList(
+        bool _useWhiteList
+        );
+
+    event AddressWhiteListed(
+        address _address
+        );
+
+    event AddressBlackListed(
+        address _address
+        );
+
+    event AdminLiquidationOrderCreated(
+        bytes32 indexed _orderId,
+        address indexed _address,
+        bytes32 indexed _marketId,
+        bool _tradeAmountGivenInShares,
+        uint256 _tradeAmount,
+        bool _tradeDirection,
+        uint256 _orderLeverage
+        );
+
     modifier onlyOracleOperator {
         require(isCallbackAddress(msg.sender), "MorpherOracle: Only the oracle operator can call this function.");
+        _;
+    }
+
+    modifier onlyAdministrator {
+        require(msg.sender == state.getAdministrator(), "Function can only be called by the Administrator.");
         _;
     }
 
@@ -106,8 +159,9 @@ contract MorpherOracle is Ownable {
         _;
     }
 
-   constructor(address _tradeEngineAddress, address _callBackAddress, address payable _gasCollectionAddress, uint256 _gasForCallback, address _coldStorageOwnerAddress) public {
+   constructor(address _tradeEngineAddress, address _morpherState, address _callBackAddress, address payable _gasCollectionAddress, uint256 _gasForCallback, address _coldStorageOwnerAddress) public {
         setTradeEngineAddress(_tradeEngineAddress);
+        setStateAddress(_morpherState);
         enableCallbackAddress(_callBackAddress);
         setCallbackCollectionAddress(_gasCollectionAddress);
         setGasForCallback(_gasForCallback);
@@ -121,6 +175,11 @@ contract MorpherOracle is Ownable {
     function setTradeEngineAddress(address _address) public onlyOwner {
         tradeEngine = MorpherTradeEngine(_address);
         emit LinkTradeEngine(_address);
+    }
+
+    function setStateAddress(address _address) public onlyOwner {
+        state = MorpherState(_address);
+        emit LinkMorpherState(_address);
     }
 
     function setGasForCallback(uint256 _gasForCallback) public onlyOwner {
@@ -147,6 +206,35 @@ contract MorpherOracle is Ownable {
         emit CallBackCollectionAddressChange(_address);
     }
 
+    function getAdministrator() public view returns(address _administrator) {
+        return state.getAdministrator();
+    }
+
+// ----------------------------------------------------------------------------------
+// Oracle Owner can use a whitelist and authorize individual addresses
+// ----------------------------------------------------------------------------------
+    function setUseWhiteList(bool _useWhiteList) public onlyOracleOperator {
+        useWhiteList = _useWhiteList;
+        emit SetUseWhiteList(_useWhiteList);
+    }
+
+    function setWhiteList(address _whiteList) public onlyOracleOperator {
+        whiteList[_whiteList] = true;
+        emit AddressWhiteListed(_whiteList);
+    }
+
+    function setBlackList(address _blackList) public onlyOracleOperator {
+        whiteList[_blackList] = false;
+        emit AddressBlackListed(_blackList);
+    }
+
+    function isWhiteListed(address _address) public view returns (bool _whiteListed) {
+        if (useWhiteList == false ||  whiteList[_address] == true) {
+            _whiteListed = true;
+        }
+        return(_whiteListed);
+    }
+
 // ----------------------------------------------------------------------------------
 // emitOrderFailed
 // Can be called by Oracle Operator to notifiy user of failed order
@@ -158,7 +246,11 @@ contract MorpherOracle is Ownable {
         bool _tradeAmountGivenInShares,
         uint256 _tradeAmount,
         bool _tradeDirection,
-        uint256 _orderLeverage
+        uint256 _orderLeverage,
+        uint256 _onlyIfPriceBelow,
+        uint256 _onlyIfPriceAbove,
+        uint256 _goodFrom,
+        uint256 _goodUntil
     ) public onlyOracleOperator {
         emit OrderFailed(
             _orderId,
@@ -167,7 +259,11 @@ contract MorpherOracle is Ownable {
             _tradeAmountGivenInShares,
             _tradeAmount,
             _tradeDirection,
-            _orderLeverage);
+            _orderLeverage,
+            _onlyIfPriceBelow,
+            _onlyIfPriceAbove,
+            _goodFrom,
+            _goodUntil);
     }
 
 // ----------------------------------------------------------------------------------
@@ -179,13 +275,22 @@ contract MorpherOracle is Ownable {
         bool _tradeAmountGivenInShares,
         uint256 _tradeAmount,
         bool _tradeDirection,
-        uint256 _orderLeverage
+        uint256 _orderLeverage,
+        uint256 _onlyIfPriceAbove,
+        uint256 _onlyIfPriceBelow,
+        uint256 _goodUntil,
+        uint256 _goodFrom
         ) public payable notPaused returns (bytes32 _orderId) {
+        require(isWhiteListed(msg.sender),"MorpherOracle: Address not eligible to create an order.");
         if (gasForCallback > 0) {
             require(msg.value >= gasForCallback, "MorpherOracle: Must transfer gas costs for Oracle Callback function.");
             callBackCollectionAddress.transfer(msg.value);
         }
         _orderId = tradeEngine.requestOrderId(msg.sender, _marketId, _tradeAmountGivenInShares, _tradeAmount, _tradeDirection, _orderLeverage);
+        priceAbove[_orderId] = _onlyIfPriceAbove;
+        priceBelow[_orderId] = _onlyIfPriceBelow;
+        goodFrom[_orderId]   = _goodFrom;
+        goodUntil[_orderId]  = _goodUntil;
         emit OrderCreated(
             _orderId,
             msg.sender,
@@ -193,25 +298,61 @@ contract MorpherOracle is Ownable {
             _tradeAmountGivenInShares,
             _tradeAmount,
             _tradeDirection,
-            _orderLeverage
+            _orderLeverage,
+            _onlyIfPriceBelow,
+            _onlyIfPriceAbove,
+            _goodFrom,
+            _goodUntil
             );
         return _orderId;
     }
 
 // ----------------------------------------------------------------------------------
 // cancelOrder(bytes32  _orderId)
-// Users can cancel their own orders before the _callback has been executed
+// User or Administrator can cancel their own orders before the _callback has been executed
 // ----------------------------------------------------------------------------------
     function cancelOrder(bytes32 _orderId) public {
         tradeEngine.cancelOrder(_orderId, msg.sender);
+        clearOrderConditions(_orderId);
         emit OrderCancelled(
             _orderId,
             msg.sender
             );
     }
 
+// ------------------------------------------------------------------------
+// checkOrderConditions(bytes32 _orderId, uint256 _price)
+// Checks if callback satisfies the order conditions
+// ------------------------------------------------------------------------
+    function checkOrderConditions(bytes32 _orderId, uint256 _price) public view returns (bool _conditionsMet) {
+        _conditionsMet = true;
+        if (now > goodUntil[_orderId] && goodUntil[_orderId] > 0) {
+            _conditionsMet = false;
+        }
+        if (now < goodFrom[_orderId] && goodFrom[_orderId] > 0) {
+            _conditionsMet = false;
+        }
+        if (_price < priceAbove[_orderId] && priceAbove[_orderId] > 0) {
+            _conditionsMet = false;
+        }
+        if (_price > priceBelow[_orderId] && priceBelow[_orderId] > 0) {
+            _conditionsMet = false;
+        }
+        return _conditionsMet;
+    }
+
 // ----------------------------------------------------------------------------------
-// Setter/getter functions for pausing the Oracle contract
+// Deletes parameters of cancelled or processed orders
+// ----------------------------------------------------------------------------------
+    function clearOrderConditions(bytes32 _orderId) internal {
+        priceAbove[_orderId] = 0;
+        priceBelow[_orderId] = 0;
+        goodFrom[_orderId]   = 0;
+        goodUntil[_orderId]  = 0;
+    }
+
+// ----------------------------------------------------------------------------------
+// Pausing/unpausing the Oracle contract
 // ----------------------------------------------------------------------------------
     function pauseOracle() public onlyOwner {
         paused = true;
@@ -249,21 +390,38 @@ contract MorpherOracle is Ownable {
     function __callback(
         bytes32 _orderId,
         uint256 _price,
+        uint256 _adjustedPrice,
         uint256 _spread,
         uint256 _liquidationTimestamp,
         uint256 _timeStamp
         ) public onlyOracleOperator notPaused returns (uint256 _newLongShares, uint256 _newShortShares, uint256 _newMeanEntry, uint256 _newMeanSpread, uint256 _newMeanLeverage, uint256 _liquidationPrice)  {
-        (
-            _newLongShares,
-            _newShortShares,
-            _newMeanEntry,
-            _newMeanSpread,
-            _newMeanLeverage,
-            _liquidationPrice
-        ) = tradeEngine.processOrder(_orderId, _price, _spread, _liquidationTimestamp, _timeStamp);
+        require(checkOrderConditions(_orderId, _price));
+        if (_adjustedPrice > 0) {
+            // There has been a stock split, dividend, contract roll or similar, and the Oracle Operator adjusted the market price accordingly
+            (
+                _newLongShares,
+                _newShortShares,
+                _newMeanEntry,
+                _newMeanSpread,
+                _newMeanLeverage,
+                _liquidationPrice
+            ) = tradeEngine.processOrder(_orderId, _adjustedPrice, _spread, _liquidationTimestamp, _timeStamp);
+        } else {
+            // Adjusted price hasn't been set by Oracle Operator, use regular _price
+            (
+                _newLongShares,
+                _newShortShares,
+                _newMeanEntry,
+                _newMeanSpread,
+                _newMeanLeverage,
+                _liquidationPrice
+            ) = tradeEngine.processOrder(_orderId, _price, _spread, _liquidationTimestamp, _timeStamp);
+        }
+        clearOrderConditions(_orderId);
         emit OrderProcessed(
             _orderId,
             _price,
+            _adjustedPrice,
             _spread,
             _liquidationTimestamp,
             _timeStamp,
@@ -277,6 +435,47 @@ contract MorpherOracle is Ownable {
         return (_newLongShares, _newShortShares, _newMeanEntry, _newMeanSpread, _newMeanLeverage, _liquidationPrice);
     }
 
+// ----------------------------------------------------------------------------------
+// delistMarket(bytes32 _marketId)
+// Administrator closes out all existing positions on _marketId market at current prices
+// ----------------------------------------------------------------------------------
+    function delistMarket(bytes32 _marketId, uint256 _fromIx, uint256 _toIx) public onlyAdministrator {
+        require(state.getMarketActive(_marketId) == true, "Market must be active to process position liquidations.");
+        // If no _fromIx and _toIx specified, do entire _list
+        if (_fromIx == 0) {
+            _fromIx = 1;
+        }
+        if (_toIx == 0) {
+            _toIx = state.getMaxMappingIndex(_marketId);
+        }
+        address _address;
+        for (uint256 i = _fromIx; i <= _toIx; i++) {
+            _address = state.getExposureMappingAddress(_marketId, i);
+            adminLiquidationOrder(_address, _marketId);
+        }
+    }
+
+// ----------------------------------------------------------------------------------
+// adminLiquidationOrder(address _address, bytes32 _marketId)
+// Administrator closes out an existing position of _address on _marketId market at current price
+// ----------------------------------------------------------------------------------
+    function adminLiquidationOrder(
+        address _address,
+        bytes32 _marketId
+        ) public onlyAdministrator returns (bytes32 _orderId) {
+            uint256 _positionLongShares = state.getLongShares(_address, _marketId);
+            uint256 _positionShortShares = state.getShortShares(_address, _marketId);
+            if (_positionLongShares > 0) {
+                _orderId = tradeEngine.requestOrderId(_address, _marketId, true, _positionLongShares, false, 10**8);
+                emit AdminLiquidationOrderCreated(_orderId, _address, _marketId, true, _positionLongShares, false, 10**8);
+            }
+            if (_positionShortShares > 0) {
+                _orderId = tradeEngine.requestOrderId(_address, _marketId, true, _positionShortShares, true, 10**8);
+                emit AdminLiquidationOrderCreated(_orderId, _address, _marketId, true, _positionShortShares, true, 10**8);
+            }
+            return _orderId;
+    }
+    
 // ----------------------------------------------------------------------------------
 // Auxiliary function to hash a string market name i.e.
 // "CRYPTO_BTC" => 0x0bc89e95f9fdaab7e8a11719155f2fd638cb0f665623f3d12aab71d1a125daf9;
