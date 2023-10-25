78b8c59e10046d05720e1b866369c29c19777792
Thomas Wiesner (thomaswiesner.com) <485781+tomw1808@users.noreply.github.com>
2023-10-25 09:24:41 +0200

    updated markets for migrations contract

--- helpers/activateMarket.js
+++ helpers/activateMarket.js
@@ -8,21 +8,22 @@
  * 
  * that will fetch the markets from docs/markets.js and start bulk activating them, given the private key is in the .env file etc...
  */
-module.exports = async function (MorpherAdministratorProxy) {
+const MorpherAdministratorProxy = artifacts.require("MorpherAdministratorProxy")
+module.exports = async function (callback) {
+
+  const accounts = await web3.eth.getAccounts()
   const marketHashObject = require('../docs/markets')
 
   const marketHashes = Object.keys(marketHashObject)
-  const adminOverrideProxy = await MorpherAdministratorProxy.deployed()
+  const adminOverrideProxy = await MorpherAdministratorProxy.at("0x4dFe1265Ed4893FB32238d577fC26535FbB59fa8")
   let marketsToAdd = []
-  for (let i = 0; i < marketHashes.length; i++) {
-    marketsToAdd.push(marketHashes[i])
-    if (marketsToAdd.length == 20) {
-      await adminOverrideProxy.bulkActivateMarkets(marketsToAdd, {
-        from: accounts[0],
-      })
-      marketsToAdd = []
-      console.log('Added 20 Markets')
-    }
+  for (let i = 0; i < marketHashes.length; i++) { 
+    marketsToAdd.push(marketHashes[i]); 
+    if (marketsToAdd.length == 20) { 
+      await adminOverrideProxy.bulkActivateMarkets(marketsToAdd, { from: accounts[0], }); 
+      marketsToAdd = []; 
+      console.log('Added 20 Markets'); 
+    } 
   }
   if (marketsToAdd.length > 0) {
     await adminOverrideProxy.bulkActivateMarkets(marketsToAdd, {
@@ -31,4 +32,5 @@ module.exports = async function (MorpherAdministratorProxy) {
     console.log('Added', marketsToAdd.length, 'Markets')
     marketsToAdd = []
   }
+  callback();
 }
