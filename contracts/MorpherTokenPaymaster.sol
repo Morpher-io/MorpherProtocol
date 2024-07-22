566a7484a9fdabd818b4edd713368c8c3ac4d0c0
Thomas Wiesner (thomaswiesner.com) <485781+tomw1808@users.noreply.github.com>
2024-07-22 14:20:40 +0200

    updated uniswap-v3-periphery lib to 0.8

--- contracts/MorpherTokenPaymaster.sol
+++ contracts/MorpherTokenPaymaster.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.15;
 
 // Import the required libraries and contracts
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
