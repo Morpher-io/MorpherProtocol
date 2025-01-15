3196d6f02e20972729729813673d6f4d5de384e7
Thomas Wiesner <485781+tomw1808@users.noreply.github.com>
2025-01-15 14:04:33 +0000

    feat: Enhance market activation script with batch processing and access control

--- scripts/helpers/EnableMarkets.s.sol
+++ scripts/helpers/EnableMarkets.s.sol
@@ -4,47 +4,56 @@ pragma solidity ^0.8.20;
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
 import {stdJson} from "forge-std/StdJson.sol";
+import {MorpherAccessControl} from "../../contracts/MorpherAccessControl.sol";
 import {MorpherAdmin} from "../../contracts/MorpherAdmin.sol";
 import {DeployOrUpgrade} from "../deployOrUpgrade.sol";
+
+//pre processing of the market ids: 
+// jq '[.[].id]' deployments/markets.json > deployments/market_ids.json
 
 contract EnableMarkets is DeployOrUpgrade {
     using stdJson for string;
 
+
     function run() public {
         // Read and process markets.json
+        vm.startBroadcast();
 
         // Deploy or upgrade MorpherAdmin
         address existingAdmin = loadAddress("MorpherAdmin");
         require(existingAdmin != address(0x0), "MorpherAdmin must be deployed for this network");
 
+        MorpherAccessControl(loadAddress("MorpherAccessControl")).grantRole(keccak256("ADMINISTRATOR_ROLE"),existingAdmin);
+
         string memory root = vm.projectRoot();
         string memory path = string.concat(root, "/deployments/market_ids.json");
         string memory json = vm.readFile(path);
         string[] memory marketIds = abi.decode(
-            json.parseRaw(""),
+            vm.parseJson(json, ""),
             (string[])
         );
 
-        // Process markets in batches of 20
-        bytes32[] memory marketsToAdd = new bytes32[](20);
+        console.log(marketIds[19]);
+
+       
         uint256 batchCount = 0;
         
+        bytes32[] memory marketsToAdd = new bytes32[](20);
         for (uint256 i = 0; i < marketIds.length; i++) {
             // Add market to current batch
-            marketsToAdd[i % 20] = keccak256(bytes(marketIds[i]));
+             // Process markets in batches of 20
+            marketsToAdd[i % 20] = keccak256(abi.encodePacked(marketIds[i]));
             
             // When batch is full or we're at the end, process it
             if ((i + 1) % 20 == 0 || i == marketIds.length - 1) {
-                uint256 batchSize = (i % 20) + 1;
-                bytes32[] memory currentBatch = new bytes32[](batchSize);
-                for (uint256 j = 0; j < batchSize; j++) {
-                    currentBatch[j] = marketsToAdd[j];
-                }
-                
-                MorpherAdmin(existingAdmin).bulkActivateMarkets(currentBatch);
+                console.logBytes32(marketsToAdd[19]);
+                MorpherAdmin(existingAdmin).bulkActivateMarkets(marketsToAdd);
                 console.log("Added batch", batchCount);
                 batchCount++;
+                marketsToAdd = new bytes32[](20);
             }
         }
+
+        vm.stopBroadcast();
     }
 }
