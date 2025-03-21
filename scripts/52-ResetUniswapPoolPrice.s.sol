e84eda95e108f2a7183a36afc82a18661a38c554
Thomas Wiesner <485781+tomw1808@users.noreply.github.com>
2025-03-21 09:15:03 +0000

    fix: Update Uniswap V3 swap router address for Base Sepolia

--- scripts/52-ResetUniswapPoolPrice.s.sol
+++ scripts/52-ResetUniswapPoolPrice.s.sol
@@ -71,7 +71,7 @@ contract ResetUniswapPoolPrice is DeployOrUpgrade {
             // Base Sepolia
             UNISWAP_V3_FACTORY = 0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24;
             NONFUNGIBLE_POSITION_MANAGER = 0x27F971cb582BF9E50F397e4d29a5C7A34f11faA2;
-            SWAP_ROUTER = 0x2626664c2603336E57B271c5C0b26F421741e481;
+            SWAP_ROUTER = 0x94cC0AaC535CCDB3C01d6787D6413C739ae12bc4;
         } else {
             revert("Unsupported chain ID");
         }
