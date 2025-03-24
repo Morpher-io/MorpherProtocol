3267e95eeb8fa3e097b8a09fdf0ece222a127250
Thomas Wiesner <485781+tomw1808@users.noreply.github.com>
2025-03-24 10:42:15 +0000

    fix: Correct zeroForOne calculation in executeSwap function

--- contracts/MorpherOracle.sol
+++ contracts/MorpherOracle.sol
@@ -683,7 +683,7 @@ contract MorpherOracle is Initializable, ContextUpgradeable, PausableUpgradeable
 		bytes[] memory inputs = new bytes[](1);
 		
 		// Determine if we're swapping token0 for token1 or vice versa
-		bool zeroForOne = equals(poolKeyIn.currency0, Currency.wrap(tokenIn));
+		bool zeroForOne = address(poolKey.currency0) == tokenIn;
 		
 		// Encode V4Router actions
 		bytes memory actions = abi.encodePacked(
